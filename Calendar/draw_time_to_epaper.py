3601430156fff4c446ad9443334493c67f0e23b2
Howard Deiner <howard.deiner@deinersoft.com>
2019-09-08 11:40:12 -0400

    tune up hands on clack face

--- Calendar/draw_time_to_epaper.py
+++ Calendar/draw_time_to_epaper.py
@@ -30,24 +30,24 @@ def draw_time_to_epaper(size, position_x, position_y, time, image, clock_face, l
     #print('draw_time_to_epaper hour_radians=' + str(hour_radians))
     #print('draw_time_to_epaper hour_display sin=' + str(math.sin(hour_radians)))
     #print('draw_time_to_epaper hour_display cos=' + str(math.cos(hour_radians)))
-    end_x = size*4/2 + math.sin(hour_radians)*((size*4/2)-75*4)
-    end_y = size*4/2 - math.cos(hour_radians)*((size*4/2)-75*4)
-    start_x = size*4/2 - math.sin(hour_radians)*10
-    start_y = size*4/2 + math.cos(hour_radians)*10
+    end_x = size*4/2 + math.sin(hour_radians)*((size*4/2)-100*4)
+    end_y = size*4/2 - math.cos(hour_radians)*((size*4/2)-100*4)
+    start_x = size*4/2 + math.sin(hour_radians)*(-20*4)
+    start_y = size*4/2 - math.cos(hour_radians)*(-20*4)
     #print('draw_time_to_epaper hour_display start_x=' + str(start_x))
     #print('draw_time_to_epaper hour_display start_y=' + str(start_y))
     #print('draw_time_to_epaper hour_display end_x=' + str(end_x))
     #print('draw_time_to_epaper hour_display end_y=' + str(end_y))
-    ImageDraw.Draw(space).line((start_x, start_y, (end_x, end_y)), fill=0, width=10*4)
+    ImageDraw.Draw(space).line((start_x, start_y, (end_x, end_y)), fill=0, width=12*4)
 
     #print('draw_time_to_epaper minute_degrees=' + str(minute_degrees))
     #print('draw_time_to_epaper minute_radians=' + str(minute_radians))
     #print('draw_time_to_epaper minute_display sin=' + str(math.sin(minute_radians)))
     #print('draw_time_to_epaper minutedisplay cos=' + str(math.cos(minute_radians)))
-    end_x = size*4/2 + math.sin(minute_radians)*((size*4/2)-15*4)
-    end_y = size*4/2 - math.cos(minute_radians)*((size*4/2)-15*4)
-    start_x = size*4/2 - math.sin(minute_radians)*10
-    start_y = size*4/2 + math.cos(minute_radians)*10
+    end_x = size*4/2 + math.sin(minute_radians)*((size*4/2)-30*4)
+    end_y = size*4/2 - math.cos(minute_radians)*((size*4/2)-30*4)
+    start_x = size*4/2 + math.sin(minute_radians)*(-20*4)
+    start_y = size*4/2 - math.cos(minute_radians)*(-20*4)
     #print('draw_time_to_epaper minute_display start_x=' + str(start_x))
     #print('draw_time_to_epaper minute_display start_y=' + str(start_y))
     #print('draw_time_to_epaper minute__display end_x=' + str(end_x))
